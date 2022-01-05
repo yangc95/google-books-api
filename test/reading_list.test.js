38128e7ffb62fa090f915c59b7701f46566f10c4
Christine Yang <yang.christine.95@gmail.com>
2022-01-04 19:16:56 -0500

    Added some new tests

--- test/reading_list.test.js
+++ test/reading_list.test.js
@@ -1,19 +1,34 @@
 const assert = require('assert');
 const { addToReadingList } = require('../src/reading_list');
 
-describe('addToReadingList', function() {
-  let readingList = [];
-  addToReadingList(readingList, 'Emma | Jane Austen | Simon & Schuster');
+describe('addToReadingList', () => {  
+  describe('should add one book to the reading list', () => {
+    let readingList = [];
 
-  it('should add the correct title of the book to the reading list', function() {
-    assert.equal(readingList[0].title, 'Emma')
+    it('zero books in the reading list', () => {
+      assert.equal(readingList.length, 0)
+    });
+    
+    it('one book is added to the reading list', () => {
+      addToReadingList(readingList, 'Emma | Jane Austen | Simon & Schuster');
+      assert.equal(readingList.length, 1)
+    });
   });
+
+  describe('should have the correct book information' , () => {
+    let readingList = [];
+    addToReadingList(readingList, 'Emma | Jane Austen | Simon & Schuster');
+
+    it('title is accurate', () => {
+      assert.equal(readingList[0].title, 'Emma')
+    });
+    
+    it('author is accurate', () => {
+      assert.equal(readingList[0].author, 'Jane Austen')
+    });
   
-  it('should add the correctauthor of the book to the reading list', function() {
-    assert.equal(readingList[0].author, 'Jane Austen')
-  });
-
-  it('should add the correct publisher of the book to the reading list', function() {
-    assert.equal(readingList[0].publisher, 'Simon & Schuster')
+    it('publisher is accurate', () => {
+      assert.equal(readingList[0].publisher, 'Simon & Schuster')
+    });
   });
 });
